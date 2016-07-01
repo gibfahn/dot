cf10380058d886d78b150f466b70341d4eb0c745
Gibson Fahnestock <gib@uk.ibm.com>
2016-07-01 12:45:26 +0100

    setup: add setup folder for initial machine config
    Added gitsetup.sh, which sets the git defaults for my machine.

---
+++ setup/gitsetup.sh
@@ -0,0 +1,4 @@
+git config --global push.default simple
+git config --global user.name "Gibson Fahnestock"
+git config --global user.email "gib@uk.ibm.com"
+git config --global --add core.whitespace fix
