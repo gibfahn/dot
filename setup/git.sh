84d1e2392b50edcc6e3f0bd816ee2f3168f80341
Gibson Fahnestock <gib@uk.ibm.com>
2016-09-19 17:40:14 +0100

    setup: add password caching for git

--- setup/git.sh
+++ setup/git.sh
@@ -3,3 +3,5 @@ git config --global push.default simple
 git config --global user.name "Gibson Fahnestock"
 git config --global user.email "gib@uk.ibm.com"
 git config --global --add core.whitespace fix
+git config --global credential.helper cache
+git config credential.helper 'cache --timeout=86400'
