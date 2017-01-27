6d90654843288ab6627f1cf53bea020dff5aa9d3
Gibson Fahnestock <gib@uk.ibm.com>
2017-01-27 10:12:59 +0000

    setup: add mac faster key repeat rate

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,3 +1,6 @@
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Set up fasteest key repeat rate (needs reboot)
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 8
