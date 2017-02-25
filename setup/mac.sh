1c9909eb551f321e5c4a025bd423fc8ff796b69d
Gibson Fahnestock <gibfahn@gmail.com>
2017-02-25 14:55:03 +0000

    brew: add current brew install list

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,6 +1,26 @@
+# Install xcode command line tools
 xcode-select --install
+# Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install cask (brew for GUI utils)
+brew tap caskroom/cask
 
 # Set up fasteest key repeat rate (needs reboot)
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 8
+
+
+# Brew stuff
+brew install bash zsh git rmtrash # rmtrash = move to trash
+brew install gnu-sed gnu-tar gnu-which htop perl tree wget
+brew install binutils coreutils findutils neovim tig zsh ctags
+brew install dfu-util # Ergodox
+brew install ninja python python3 ccache gdb # Build utilities
+# brew install exercism # Am I still doing this?
+# brew install mongodb redis postgresql # Not sure if I'll need these
+
+# Brew cask stuff
+brew cask install google-chrome firefox meld
+brew cask install gpgtools exa
+brew caask install docker vagrant virtualbox
+# brew cask install hammerspoon # Did I even do lua scripting?
