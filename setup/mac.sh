5db71b74b346dbafc3d42632762a8eafb713f154
Gibson Fahnestock <gibfahn@gmail.com>
2017-03-18 19:48:40 +0000

    setup: add entr and zsh helpers for mac

--- setup/mac.sh
+++ setup/mac.sh
@@ -12,10 +12,12 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 8
 
 # Brew stuff
 brew install bash zsh git rmtrash # rmtrash = move to trash
+brew install entr # Run command on file change
 brew install gnu-sed gnu-tar gnu-which htop perl tree wget
 brew install binutils coreutils findutils neovim tig zsh ctags
 brew install dfu-util # Ergodox
 brew install ninja python python3 ccache gdb # Build utilities
+# brew install fzf # Fuzzy finder used in vim
 # brew install exercism # Am I still doing this?
 # brew install mongodb redis postgresql # Not sure if I'll need these
 
@@ -24,3 +26,6 @@ brew cask install google-chrome firefox meld
 brew cask install gpgtools exa
 brew caask install docker vagrant virtualbox
 # brew cask install hammerspoon # Did I even do lua scripting?
+
+# zsh stuff, see ~/.gibrc for loading
+brew install zsh-completions zsh-syntax-highlighting
