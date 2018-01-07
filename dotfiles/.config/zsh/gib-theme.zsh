65a9b63bbb4f5c50eaee3be5b12081cb3ea93b3a
Gibson Fahnestock <gibfahn@gmail.com>
2018-01-07 23:23:23 +0000

    gibrc: add git upstream information to prompt
    - Improve branch name, so it now shows `master~1` if you're 1 commit
    behind master (and not on a branch or tag).
    
    - Add status indicators for:
      - !  -> No upstream branch set.
      - ⇡  -> branch is ahead of upstream
      - ⇣  -> branch is behind upstream
      - ⇣⇡ -> branch has diverged from upstream
    
    Refs: https://github.com/sindresorhus/pure/blob/master/pure.zsh
    Refs: https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh

--- dotfiles/.config/zsh/gib-theme.zsh
+++ dotfiles/.config/zsh/gib-theme.zsh
@@ -17,52 +17,26 @@ preexec() { unset timer_prompt; timer="${timer:-$SECONDS}"; }
 precmd() { [ "$timer" -a "$SECONDS" != "$timer" ] && timer_prompt=" $(($SECONDS - $timer))s" || unset timer_prompt; unset timer; }
 
 # Outputs current branch info in prompt format
-function git_prompt_info_gib() {
-  local ref
-  ref=$(command git symbolic-ref HEAD 2> /dev/null) || ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-  echo " %F{33}(${ref#refs/heads/}$(parse_git_dirty_gib)%f"
+git_prompt_info_gib() {
+  [ -d .git -o "$(command git rev-parse --is-inside-work-tree &>/dev/null)" ] &&
+    echo " %F{33}(%F{226}$(upstream_diff_gib)%F{33}$(command git describe --contains --all HEAD)$(parse_git_dirty_gib)%F{33})%f"
 }
 
 # Checks if working tree is dirty
-function parse_git_dirty_gib() {
-  local STATUS=''
-  local -a FLAGS
-  FLAGS=('--porcelain')
-  if [[ "$(command git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
-    if [ $POST_1_7_2_GIT -gt 0 ]; then
-      FLAGS+='--ignore-submodules=dirty'
-    fi
-    if [ "$DISABLE_UNTRACKED_FILES_DIRTY" = "true" ]; then
-      FLAGS+='--untracked-files=no'
-    fi
-    STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+parse_git_dirty_gib() {
+  [ "$(command git status --porcelain --ignore-submodules=none 2> /dev/null | tail -n1)" ] && echo "%F{226}✦"
+}
+
+upstream_diff_gib() {
+  local -a diff; diff=($(command git rev-list --left-right --count HEAD...@{upstream} 2>/dev/null))
+  if [ "$?" != 0 ]; then
+    printf !
+  # zsh arrays start at 1 not 0.
+  elif [ "${diff[1]}" != 0 -a "${diff[2]}" != 0 ]; then
+    printf ⇣⇡
+  elif [ "${diff[1]}" != 0 -a "${diff[2]}" = 0 ]; then
+    printf ⇡
+  elif [ "${diff[2]}" != 0 -a "${diff[1]}" = 0 ]; then
+    printf ⇣
   fi
-  [ -n "$STATUS" ] && echo "%F{226}✦%F{33})" || echo ")"
 }
-
-# Compares the provided version of git to the version installed and on path
-# Outputs -1, 0, or 1 if the installed version is less than, equal to, or
-# greater than the input version, respectively.
-function git_compare_version() {
-  local INPUT_GIT_VERSION INSTALLED_GIT_VERSION
-  INPUT_GIT_VERSION=(${(s/./)1})
-  INSTALLED_GIT_VERSION=($(command git --version 2>/dev/null))
-  INSTALLED_GIT_VERSION=(${(s/./)INSTALLED_GIT_VERSION[3]})
-
-  for i in {1..3}; do
-    if [[ $INSTALLED_GIT_VERSION[$i] -gt $INPUT_GIT_VERSION[$i] ]]; then
-      echo 1
-      return 0
-    fi
-    if [[ $INSTALLED_GIT_VERSION[$i] -lt $INPUT_GIT_VERSION[$i] ]]; then
-      echo -1
-      return 0
-    fi
-  done
-  echo 0
-}
-
-# This is unlikely to change so make it all statically assigned
-POST_1_7_2_GIT=$(git_compare_version "1.7.2")
-# Clean up the namespace slightly by removing the checker function
-unfunction git_compare_version
