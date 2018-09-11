be5d769f36c2f28bf77aba4722def1106d4e442d
Gibson Fahnestock <gibfahn@gmail.com>
2018-09-11 13:27:00 +0100

    zsh: add <C-g><C-{f,b,t,r,h}> bindings for git
    <C-g><C-{f,b,t,r,h}> map to {files,branches,tags,remotes,hashes}
    
    I still need to improve the format of some things, e.g. the log.
    
    Refs: https://junegunn.kr/2016/07/fzf-git

--- dotfiles/.config/zsh/completion-keybindings.zsh
+++ dotfiles/.config/zsh/completion-keybindings.zsh
@@ -1,3 +1,46 @@
+# Fzf git files (unstaged).
+_gib_git_f() {
+  git -c color.status=always status --short |
+  fzf --height 50% "$@" --border -m --ansi --nth 2..,.. \
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' | cut -c4- | sed 's/.* -> //'
+}
+# Fzf git branches.
+_gib_git_b() {
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  fzf --height 50% "$@" --border --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES | sed 's/^..//' | cut -d' ' -f1 | sed 's#^remotes/##'
+}
+# Fzf git tags.
+_gib_git_t() {
+  git tag --sort -version:refname |
+  fzf --height 50% "$@" --border --multi --preview-window right:70% \
+    --preview 'git show --color=always {} | head -'$LINES
+}
+# Fzf git hashes.
+_gib_git_h() {
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf --height 50% "$@" --border --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES | grep -o "[a-f0-9]\{7,\}"
+}
+# Fzf git remotes.
+_gib_git_r() {
+  git remote -v | awk '{print $1 "\t" $2}' | uniq |
+  fzf --height 50% "$@" --border --tac \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+  cut -d$'\t' -f1
+}
+# More fzf helpers.
+_gib_join-lines() { local item; while read item; do echo -n "${(q)item} "; done; }
+bind-git-helper() {
+  local c
+  for c in $@; do
+    eval "_gib_fzf-g$c-widget() { git rev-parse HEAD > /dev/null 2>&1 || return; local result=\$(_gib_git_$c | _gib_join-lines); zle reset-prompt; LBUFFER+=\$result }"
+    eval "zle -N _gib_fzf-g$c-widget"
+    eval "bindkey -M viins '^g^$c' _gib_fzf-g$c-widget"
+  done
+}
+
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} r:|[.,_-]=*'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}" # Use LS_COLORS in file completion menu.
 zstyle ':completion:*:*:*:*:*' menu "select" # Make the completion selection menu a proper menu.
@@ -41,3 +84,8 @@ bindkey -M main "^[[B" history-beginning-search-forward-end  # Re-enable down fo
 bindkey ' ' magic-space # <Space> = do history expansion
 bindkey "${terminfo[kcbt]}" reverse-menu-complete   # <Shift>-<Tab> - move backwards through the completion menu.
 bindkey -M vicmd ' ' edit-command-line # <Space> in cmd mode opens editor.
+
+# Bind git shortcuts to <c-g><c-$@> (see above functions for more info).
+bindkey -r -M viins "^G" # Remove list-expand binding so we can use <C-g> for git.
+bind-git-helper f b t r h # Bind <C-g><C-{f,b,t,r,h}> to fuzzy-find show {files,branches,tags,remotes,hashes}.
+unset -f bind-git-helper
