30a78e5aef50e5370ac7c5d9837201b0b025c82d
Gibson Fahnestock <gibfahn@gmail.com>
2019-01-20 18:58:56 +0000

    gibrc: remove colour variables from `env` output
    - Don't export `$LS_COLORS` and `$LSCOLORS` for ls, just have them as
      local variables (visible with `set`) and pass them to ls in the alias.
    - Don't export the `$LESS_TERMCAP_*` variables, as they color the output
      of `env`. Instead generate a lesskey file and write them there in a
      setup script. Don't do this for the `$LESS` variable, because it's
      worth having those settings visible in the environment (IMO).
    - Remove the BLUEMIX_HOME setting as I'm unlikely to use that again.
    - Remove the nvm special-casing for non-x86 platforms.
    
    Refs: https://unix.stackexchange.com/questions/328597/how-to-make-lesskey-terminal-independent
    Refs: https://unix.stackexchange.com/questions/108699/documentation-on-less-termcap-variables
    Refs: https://linux.die.net/man/1/lesskey

--- setup/unix.sh
+++ setup/unix.sh
@@ -134,6 +134,28 @@ mkdir -p "$XDG_DATA_HOME/zfunc/source"
 # Set up zsh scripts:
 mkdir -p "$XDG_DATA_HOME/zsh"
 
+# Set up less file (colors for less).
+# We don't save the input to a lesskey file because we need the terminal to
+# resolve the tput commands.
+if exists lesskey; then
+  update "Lesskey"
+  lesskey -o "$XDG_CACHE_HOME/less" <(
+cat << EOF
+$(echo "#env") # Get a comment.
+LESS_TERMCAP_md = $(tput bold; tput setaf 6)
+LESS_TERMCAP_me = $(tput sgr0)
+LESS_TERMCAP_so = $(tput bold; tput setaf 3; tput setab 4)
+LESS_TERMCAP_se = $(tput rmso; tput sgr0)
+LESS_TERMCAP_us = $(tput smul; tput bold; tput setaf 7)
+LESS_TERMCAP_ue = $(tput rmul; tput sgr0)
+LESS_TERMCAP_mr = $(tput rev)
+LESS_TERMCAP_mh = $(tput dim)
+EOF
+  )
+else
+  skip "Lesskey"
+fi
+
 gitCloneOrUpdate zsh-users/zsh-syntax-highlighting "$XDG_DATA_HOME/zsh/zsh-syntax-highlighting"
 
 # Install or update pip modules.
