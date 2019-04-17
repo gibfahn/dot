4ec64fa95f0ca67e760a4f1d6ad5a63ce6085894
Gibson Fahnestock <gibfahn@gmail.com>
2019-04-17 09:58:59 +0100

    slack: update slack css url to one that colors threads
    Previous one was broken by changes to slack.

--- setup/optional/slack-black.js
+++ setup/optional/slack-black.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
    $.ajax({
-      url: 'https://raw.githubusercontent.com/laCour/slack-night-mode/master/css/raw/black.css',
+      url: 'https://raw.githubusercontent.com/LanikSJ/slack-dark-mode/master/dark-theme.css',
       success: function(css) {
          $("<style></style>").appendTo('head').html(css);
       }
