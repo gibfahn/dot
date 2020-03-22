c7e6d635226ada961155c5a0f34f77f9277cd24a
Gibson Fahnestock <gibfahn@gmail.com>
2020-03-22 02:43:26 +0000

    feat(vim): update vim plugins automatically

--- setup/unix.sh
+++ setup/unix.sh
@@ -255,12 +255,17 @@ gem update "$(gem outdated | awk '{print $1}')"
 
 # Install vim-plug (vim plugin manager):
 if no nvim/site/autoload/plug.vim; then
+  log_get "Installing vim"
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   unset VIM
   { exists nvim && VIM=nvim; } || { exists vim && VIM=vim; } # Take what you can get.
   [[ "$VIM" = vim ]] && mkdir -p ~/.vim && ln -s ~/.local/share/nvim/site/autoload ~/.vim/autoload
   exists $VIM && $VIM +PlugInstall +qall # Install/update vim plugins.
+else
+  log_update "Vim Plugins"
+  { exists nvim && VIM=nvim; } || { exists vim && VIM=vim; } # Take what you can get.
+  exists $VIM && $VIM -N -c 'PlugUpgrade | PlugClean | PlugUpdate' -c quitall -e -s -V1
 fi
 
 # Symlink fzf
