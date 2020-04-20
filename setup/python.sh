215b81110a73f90f62f4b067be59dc3ecfecb776
Gibson Fahnestock <gibfahn@gmail.com>
2020-04-20 21:25:22 +0100

    perf(up): split out python updates

---
+++ setup/python.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+. "$(dirname "$0")"/../helpers/setup.sh # Load helper script from dot/helpers.
+
+set -ex
+
+pip3_modules=(
+  neovim-remote                 # Connect to existing nvim sessions (try `g cm` in a nvim terminal).
+  pynvim                        # Python plugin framework for neovim.
+  'python-language-server[all]' # Python language server (I use it in neovim).
+  vim-vint                      # Vim linter.
+)
+
+# Install or update pip modules.
+export PATH=/usr/local/bin:$PATH # Make sure brew pip/ruby are the first pip/ruby in the PATH.
+pip=pip
+exists pip3 && pip=pip3
+pip_installed="$($pip list | awk '{print $1}')"
+pip_outdated="$($pip list --outdated | awk '{print $1}')"
+
+for module in "${pip3_modules[@]}"; do
+  if ! echo "$pip_installed" | grep -qx "${module%[*}" \
+      || echo "$pip_outdated" | grep -qx "${module%[*}"; then
+    # log_get "$pip: $module"
+    $pip install -Uq "$module"
+  else
+    :
+    # TODO(gib): add skipping in the check command.
+    # log_skip "$pip: $module"
+  fi
+done
--- setup/unix.sh
+++ setup/unix.sh
@@ -16,13 +16,6 @@ npm_modules=(
   yarn                              # Alternative to npm.
 )
 
-pip3_modules=(
-  neovim-remote                 # Connect to existing nvim sessions (try `g cm` in a nvim terminal).
-  pynvim                        # Python plugin framework for neovim.
-  'python-language-server[all]' # Python language server (I use it in neovim).
-  vim-vint                      # Vim linter.
-)
-
 ruby_gems=(
   solargraph                    # Ruby LanguageServer Client.
 )
@@ -150,23 +143,6 @@ zinit update --all -p 20
 # zinit delete --clean -y # Remove no-longer-used plugins and snippets.
 # zinit cclear # Remove outdated completion entries.
 '
-
-# Install or update pip modules.
-export PATH=/usr/local/bin:$PATH # Make sure brew pip/ruby are the first pip/ruby in the PATH.
-pip=pip
-exists pip3 && pip=pip3
-pip_installed="$($pip list | awk '{print $1}')"
-pip_outdated="$($pip list --outdated | awk '{print $1}')"
-
-for module in "${pip3_modules[@]}"; do
-  if ! echo "$pip_installed" | grep -qx "${module%[*}" \
-      || echo "$pip_outdated" | grep -qx "${module%[*}"; then
-    log_get "$pip: $module"
-    $pip install -Uq "$module"
-  else
-    log_skip "$pip: $module"
-  fi
-done
 
 # Install nvm:
 unamem="$(uname -m)"
