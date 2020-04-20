2c5b968848fa2159c0b88df7038514ba75db7394
Gibson Fahnestock <gibfahn@gmail.com>
2020-04-20 21:57:41 +0100

    perf(up): split out vim update into a separate task

--- setup/unix.sh
+++ setup/unix.sh
@@ -122,21 +122,6 @@ zinit update --all -p 20
 # zinit cclear # Remove outdated completion entries.
 '
 
-# Install vim-plug (vim plugin manager):
-if no nvim/site/autoload/plug.vim; then
-  log_get "Installing vim"
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  unset VIM
-  { exists nvim && VIM=nvim; } || { exists vim && VIM=vim; } # Take what you can get.
-  [[ "$VIM" = vim ]] && mkdir -p ~/.vim && ln -s ~/.local/share/nvim/site/autoload ~/.vim/autoload
-  exists $VIM && $VIM +PlugInstall +qall # Install/update vim plugins.
-else
-  log_update "Vim Plugins"
-  { exists nvim && VIM=nvim; } || { exists vim && VIM=vim; } # Take what you can get.
-  exists $VIM && $VIM -N -u "$XDG_CONFIG_HOME"/nvim/init.vim -c 'PlugUpgrade | PlugClean | PlugUpdate' -c quitall -e -s
-fi
-
 # Symlink fzf
 if not fzf && [[ -d "$XDG_DATA_HOME"/fzf/bin/ ]]; then
   ln -sf "$XDG_DATA_HOME"/fzf/bin/* "$HOME"/bin/
