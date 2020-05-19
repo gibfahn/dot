a8e76ae16dae34edb6a3ffafed29d3db8bd1f603
Gibson Fahnestock <gibfahn@gmail.com>
2020-05-20 00:19:05 +0100

    feat(less): update lesskey colours
    Make things prettier.
    
    Refs: https://unix.stackexchange.com/questions/119/colors-in-man-pages/147#147
    Refs: https://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/

--- setup/run_less.sh
+++ setup/run_less.sh
@@ -8,13 +8,27 @@ set -ex
   lesskey -o "$XDG_CACHE_HOME/less" <(
 cat << EOF
 $(echo "#env") # Get a comment.
-LESS_TERMCAP_md = $(tput bold; tput setaf 6)
+
+LESS_TERMCAP_ZN = $(tput ssubm)
+LESS_TERMCAP_ZO = $(tput ssupm)
+LESS_TERMCAP_ZV = $(tput rsubm)
+LESS_TERMCAP_ZW = $(tput rsupm)
+# green
+LESS_TERMCAP_mb = $(tput bold; tput setaf 2)
+# Blue.
+LESS_TERMCAP_md = $(tput bold; tput setaf 74)
 LESS_TERMCAP_me = $(tput sgr0)
-LESS_TERMCAP_so = $(tput bold; tput setaf 3; tput setab 4)
-LESS_TERMCAP_se = $(tput rmso; tput sgr0)
-LESS_TERMCAP_us = $(tput smul; tput bold; tput setaf 7)
-LESS_TERMCAP_ue = $(tput rmul; tput sgr0)
-LESS_TERMCAP_mr = $(tput rev)
+# Dim (half-bright) mode.
 LESS_TERMCAP_mh = $(tput dim)
+# Reverse mode.
+LESS_TERMCAP_mr = $(tput rev)
+# End standout-mode.
+LESS_TERMCAP_se = $(tput rmso; tput sgr0)
+# Begin standout-mode - info box (yellow on blue).
+LESS_TERMCAP_so = $(tput bold; tput setaf 3; tput setab 4)
+ # End underline.
+LESS_TERMCAP_ue = $(tput rmul; tput sgr0)
+# Begin underline (white).
+LESS_TERMCAP_us = $(tput smul; tput bold; tput setaf 146)
 EOF
   )
