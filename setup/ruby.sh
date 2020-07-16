72bd1523a3cf1958b37f7be9d10c09c9f6df00aa
Gibson Fahnestock <gibfahn@gmail.com>
2020-07-16 17:19:24 +0100

    fix(ruby): install ruby before installing gems

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -8,6 +8,20 @@ ruby_gems=(
   solargraph                    # Ruby LanguageServer Client.
 )
 
+# Add rbenv to path in case it was only just installed.
+if [[ $USER == gib ]] && not rbenv; then
+  export PATH="$XDG_DATA_HOME/rbenv/bin:$PATH"
+  export PATH="$XDG_CACHE_HOME/rbenv/shims:$PATH"
+  export RBENV_ROOT="${RBENV_ROOT:-"$XDG_CACHE_HOME/rbenv"}" # Set rbenv location.
+fi
+
+# Install latest version of ruby if changed.
+[[ $USER == gib ]] && {
+  latest_ruby_version=$(rbenv install --list | awk '/^\s*[0-9]+\.[0-9]+\.[0-9]+\s*$/ {a=$1} END { print a }')
+  rbenv install --skip-existing "$latest_ruby_version"
+  rbenv global "$latest_ruby_version"
+}
+
 # Install ruby gems
 if [[ $USER == gib ]]; then
   for gem in "${ruby_gems[@]}"; do
@@ -22,17 +36,3 @@ fi
 
 # Update ruby gems.
 gem update "$(gem outdated | awk '{print $1}')"
-
-# Add rbenv to path in case it was only just installed.
-if [[ $USER == gib ]] && not rbenv; then
-  export PATH="$XDG_DATA_HOME/rbenv/bin:$PATH"
-  export PATH="$XDG_CACHE_HOME/rbenv/shims:$PATH"
-  export RBENV_ROOT="${RBENV_ROOT:-"$XDG_CACHE_HOME/rbenv"}" # Set rbenv location.
-fi
-
-# Install latest version of ruby if changed.
-[[ $USER == gib ]] && {
-  latest_ruby_version=$(rbenv install --list | awk '/^\s*[0-9]+\.[0-9]+\.[0-9]+\s*$/ {a=$1} END { print a }')
-  rbenv install --skip-existing "$latest_ruby_version"
-  rbenv global "$latest_ruby_version"
-}
