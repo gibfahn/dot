217c2ca280847b56fde39005af492ec4cbdd8657
Gibson Fahnestock <gibfahn@gmail.com>
2020-09-17 14:30:35 +0100

    feat(zsh): create hermetic_zinit script

--- setup/zinit.sh
+++ setup/zinit.sh
@@ -6,14 +6,8 @@ set -ex
 
 # Update zsh plugins.
 gitCloneOrUpdate zdharma/zinit "$XDG_DATA_HOME/zsh/zinit/bin" # Zsh plugin manager.
-zsh -c '
-declare -A ZINIT
-ZINIT[HOME_DIR]=$XDG_DATA_HOME/zsh/zinit
-ZINIT[BIN_DIR]=$XDG_DATA_HOME/zsh/zinit/bin # Where zinit is installed.
-ZINIT[ZCOMPDUMP_PATH]=$XDG_CACHE_HOME/zsh/.zcompdump$(hostname)
-source "$XDG_DATA_HOME/zsh/zinit/bin/zinit.zsh" # Source plugin manager.
-zinit self-update
-zinit update --all -p 20
-# zinit delete --clean -y # Remove no-longer-used plugins and snippets.
-# zinit cclear # Remove outdated completion entries.
-'
+
+hermetic_zinit self-update
+hermetic_zinit update --all -p 20
+# hermetic_zinit delete --clean -y # Remove no-longer-used plugins and snippets.
+# hermetic_zinit cclear # Remove outdated completion entries.
