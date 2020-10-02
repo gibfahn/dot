3d5b625b8ab1d4209cd2a2e238362a7d89da6bb6
Gibson Fahnestock <gibfahn@gmail.com>
2020-10-02 14:19:44 +0100

    refactor(macOS): make login items script callable
    Allows calling it from other dotfile scripts.

--- setup/login_items.sh
+++ setup/login_items.sh
@@ -3,13 +3,14 @@
 set -e
 
 # Login Items that should be added to System Preferences -> Users & Groups -> Login Items
-required_login_items=(Rectangle CopyQ Hammerspoon "Time Out")
+required_login_items=("$@")
 
 # Set up login items.
 IFS=$'\n' current_login_items=($(osascript -e 'tell application "System Events" to get login items' | sed -e 's/login item //g' -e 's/, /\n/g'))
-for item in "${required_login_items[@]}"; do
+for item_path in "${required_login_items[@]}"; do
+  item=$(basename "${item_path%.app}")
   if ! (( ${current_login_items[(I)$item]} )); then
     echo "Adding login item $item" >&2
-    osascript -e "tell application \"System Events\" to make login item at end with properties {path:\"/Applications/$item.app\", hidden:false}"
+    osascript -e "tell application \"System Events\" to make login item at end with properties {path:\"$item_path\", hidden:false}"
   fi
 done
