abf9a33cf9e45f94dc9f4ed321c0a8b505d4b3a9
Gibson Fahnestock <gibfahn@gmail.com>
2020-12-24 16:41:22 +0000

    fix(zsh): fix adding git directories to z history
    This wasn't working because I missed the `!`, so was only adding
    entries that were already present to the z config.

--- setup/git.sh
+++ setup/git.sh
@@ -5,7 +5,7 @@ set -eu
 # Things that I want to do to every git repository, every time.
 
 # If a repo isn't in $_Z_DATA then add it.
-if grep -q "^$PWD|" "$_Z_DATA"; then
+if ! grep -q "^$PWD|" "$_Z_DATA"; then
   echo >&2 "z: adding $PWD to $_Z_DATA"
   echo "$PWD|10|$(date +%s)" >> "$_Z_DATA"
 fi
