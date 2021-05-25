8111ad54993bd8d7008866f2c3a008a51eaaed6e
Gibson Fahnestock <gibfahn@gmail.com>
2021-05-25 09:45:47 +0100

    fix(hammerspoon): remap keys again on unlock and wake from sleep
    Turns out Hammerspoon has a nice module for doing this.

--- dotfiles/.config/hammerspoon/init.lua
+++ dotfiles/.config/hammerspoon/init.lua
@@ -1,18 +1,44 @@
--- local log = hs.logger.new('init.lua', 'debug')
+local log = hs.logger.new('init.lua', 'debug')
 -- Use Control+` to reload Hammerspoon config
 hs.hotkey.bind({'ctrl'}, '`', nil, function() hs.reload() end)
 
--- Run key remapping script, used in other hyper.lua too.
+-- Run key remapping script, used in hyper.lua too.
 RemapKeys = function()
     local cmd = os.getenv("HOME") .. "/bin/hid"
     local output, _, _, rc = hs.execute(cmd)
     hs.hid.capslock.set(false) -- Turn off Caps Lock.
     hs.notify.new({
-        title = 'Running Hidutil...',
+        title = 'Remapping Keys (running hidutil)...',
         informativeText = rc .. " " .. output,
         withdrawAfter = 3
     }):send()
 end
+
+-- Always remap keys on first loading hammerspoon.
+RemapKeys()
+
+-- Always re-remap keys on some events.
+local remapKeyWatcher = hs.caffeinate.watcher.new(
+                            function(event)
+        local eventsToMatch = {
+            [hs.caffeinate.watcher.systemDidWake] = true, -- 0
+            [hs.caffeinate.watcher.systemWillSleep] = false, -- 1
+            [hs.caffeinate.watcher.systemWillPowerOff] = false, -- 2
+            [hs.caffeinate.watcher.screensDidSleep] = false, -- 3
+            [hs.caffeinate.watcher.screensDidWake] = false, -- 4
+            [hs.caffeinate.watcher.sessionDidResignActive] = false, -- 5
+            [hs.caffeinate.watcher.sessionDidBecomeActive] = true, -- 6
+            [hs.caffeinate.watcher.screensaverDidStart] = false, -- 7
+            [hs.caffeinate.watcher.screensaverWillStop] = false, -- 8
+            [hs.caffeinate.watcher.screensaverDidStop] = false, -- 9
+            [hs.caffeinate.watcher.screensDidLock] = false, -- 10
+            [hs.caffeinate.watcher.screensDidUnlock] = true -- 11
+        }
+        -- Check event number against commented numbers above.
+        log.d('Event: ', event, ' matches: ', eventsToMatch[event])
+        if eventsToMatch[event] then RemapKeys() end
+    end)
+remapKeyWatcher:start()
 
 -- {{{ § -> Run hidutil script to re-apply key mappings.
 -- This key is one I don't use, that only exists pre-mapping.
