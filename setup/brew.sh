16c49abf7db6aa33ccedb436ebcda03b69d569b6
Gibson Fahnestock <gibfahn@gmail.com>
2021-10-23 01:20:03 +0100

    fix(brew): handle the new way of updating sogouinput

--- setup/brew.sh
+++ setup/brew.sh
@@ -5,15 +5,14 @@
 set -ex
 
 # Install brew and Xcode Command Line Tools.
-if exists brew; then
-  log_skip "brew (already installed)."
-else
-  log_get "brew."
+if ! exists brew; then
   # Should only exist if we're on a work machine.
   if [[ -d $wrk_dotfile_dir ]]; then
     "$wrk_dotfile_dir"/setup/brew.sh
   else
+    { set +x; } 2>/dev/null
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    set -x
   fi
 fi
 
@@ -22,22 +21,24 @@ fi
 # - /Library/Developer/CommandLineTools
 # - /Applications/Xcode.app/Contents/Developer
 if ! xcode-select -p &>/dev/null; then
-  log_skip "Xcode Command Line Tools."
   xcode-select --install
-else
-  log_skip "Xcode Command Line Tools (already installed)."
 fi
-
-sogou_dir_old="$(ls -a "$(brew --prefix)"/Caskroom/sogouinput 2>/dev/null || true)"
 
 copyq_version_old=$(copyq --version || true)
 
 # Disable quarantine for casks.
 export HOMEBREW_CASK_OPTS=--no-quarantine
 
-log_update "Brew update && upgrade"
 # Upgrade everything, even things that weren't in your Brewfile.
 brew update
+
+if ! brew outdated sogouinput; then
+  brew reinstall sogouinput
+
+  sogou_dir="$(brew info sogouinput | awk '/\/Caskroom\/sogouinput\// { print $1 }')"
+  open -a "$sogou_dir"/sogou*.app
+fi
+
 brew upgrade # You may occasionally want to run `brew upgrade --greedy` in case built-in cask updaters aren't working.
 
 # brew install things. Edit config/Brewfile to adjust.
@@ -51,29 +52,18 @@ fi
 brewfiles=${brewfiles//
 / }
 
-if brew bundle --file=<(cat $brewfiles) check >/dev/null; then
-  log_skip "brew packages."
-else
-  log_get "brew packages."
+if ! brew bundle --file=<(cat $brewfiles) check >/dev/null; then
   brew bundle --file=<(cat $brewfiles) | grep -Evx "Using [-_/0-9a-zA-Z ]+"
 fi
 
 # Useful to see what you need to add to your Brewfiles.
-log_get "Brew packages that would be cleaned up, run this to actually clean:
+echo "Brew packages that would be cleaned up, run this to actually clean:
   brew bundle cleanup --force --file=<(cat $brewfiles)"
 brew bundle cleanup --file=<(cat $brewfiles)
 
 copyq_version_new=$(copyq --version)
 
 if [[ $copyq_version_old != "$copyq_version_new" ]]; then
-  log_error "CopyQ version changed, you need to manually untick and retick System Preferences -> Security & Privacy -> Accessibility -> CopyQ.app"
+  echo "ERROR: CopyQ version changed, you need to manually untick and retick System Preferences -> Security & Privacy -> Accessibility -> CopyQ.app"
   open /System/Library/PreferencePanes/Security.prefPane
-fi
-
-sogou_dir_new="$(ls -a "$(brew --prefix)"/Caskroom/sogouinput 2>/dev/null || true)"
-# If sogouinput was updated
-if [[ "$sogou_dir_old" != "$sogou_dir_new" ]]; then
-  log_update "Sogou Input"
-  sogou_dir="$(brew info sogouinput | awk '/\/Caskroom\/sogouinput\// { print $1 }')"
-  [[ -n "$sogou_dir" ]] && open -a "$sogou_dir"/sogou*.app
 fi
