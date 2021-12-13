b81ea4cdd7011bbe422cc5c96bd87ef1f3c4d9c3
Gibson Fahnestock <gibfahn@gmail.com>
2021-12-13 13:12:14 +0000

    fix(nvim): remove deprecated jump_to_first_match lightspeed option
    Use the default opts.labels and opts.safe_labels instead.

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -431,7 +431,6 @@ require'nvim-treesitter.configs'.setup {
 -- (in Operator-pending mode the search is invoked with z/Z not s/S)
 -- `:h lightspeed` for more info.
 require'lightspeed'.setup {
-  jump_to_first_match = true,
   jump_on_partial_input_safety_timeout = 400,
   -- This can get _really_ slow if the window has a lot of content,
   -- turn it on only if your machine can always cope with it.
