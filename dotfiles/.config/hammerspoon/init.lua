26dbffa86939a5740f2f2eda77011bcab085dab0
Gibson Fahnestock <gibfahn@gmail.com>
2022-04-25 09:30:03 +0100

    fix(hammerspoon): handle multiple users in config

--- dotfiles/.config/hammerspoon/init.lua
+++ dotfiles/.config/hammerspoon/init.lua
@@ -8,17 +8,21 @@ function RequireIfAvailable(module)
   if not (res) then log.d("Module not loaded as not found: " .. module) end
 end
 
-require('hidutil')
-require('hyper') -- Uses RemapKeys from hidutil, so require order matters.
-require('control-escape')
+local user = os.getenv("USER")
 
--- require('wrk')
-RequireIfAvailable('wrk') -- Uses RemapKeys from hidutil, so require order matters.
+require('hidutil')
+
+if (user == "gib") then
+  require('hyper') -- Uses RemapKeys from hidutil, so require order matters.
+  require('control-escape')
+
+  RequireIfAvailable('wrk') -- Uses RemapKeys from hidutil, so require order matters.
+end
 
 -- Always remap keys on first loading hammerspoon.
 RemapKeys()
 
 -- Always connect to VPN on first loading Hammerspoon.
-CallVpn("corporate")
+if (user == "gib") then CallVpn("corporate") end
 
-hs.notify.new({title = 'Hammerspoon', informativeText = 'Gib config restored 🤘', withdrawAfter = 3}):send()
+hs.notify.new({title = 'Hammerspoon', informativeText = user .. ' config restored 🤘', withdrawAfter = 3}):send()
