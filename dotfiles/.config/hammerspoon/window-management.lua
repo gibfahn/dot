fe95f1c4d61a4c766ae012107e50875efe9104fb
Gibson Fahnestock <gibfahn@gmail.com>
2022-08-30 21:06:15 +0100

    refactor(hammerspoon): extract window management I actually use

--- dotfiles/.config/hammerspoon/window-management.lua
+++ dotfiles/.config/hammerspoon/window-management.lua
@@ -1,29 +1,138 @@
 -- Replacement for Spectacle/Rectangle.
 local log = hs.logger.new('window-management.lua', 'debug')
 
--- Symlinked in in up/run/unix
-hs.loadSpoon("Lunette")
+--- Disable animation duration so resizing is instant.
+hs.window.animationDuration = 0
 
-local customBindings = {
-  leftHalf = {
-    {{"cmd", "alt"}, "h"},
-  },
-  nextDisplay = {
-    {{"cmd", "alt"}, "n"},
-  },
-  fullScreen = {
-    {{"cmd", "alt"}, "e"},
-  },
-  rightHalf = {
-    {{"cmd", "alt"}, "i"},
-  },
-  prevDisplay = false,
-  nextThird = false,
-  prevThird = false,
-  enlarge = false,
-  shrink = false,
-  undo = false,
-  redo = false,
-}
+-- Functions to check current state
 
-spoon.Lunette:bindHotkeys(customBindings)
+local is_left_half = function(window, screen)
+  return window.x == screen.x and
+         window.y == screen.y and
+         window.w == screen.w // 2 and
+         window.h == screen.h
+end
+
+local is_left_two_thirds = function(window, screen)
+  return window.x == screen.x and
+         window.y == screen.y and
+         window.w == ((screen.w // 3) * 2) and
+         window.h == screen.h
+end
+
+local is_right_half = function(window, screen)
+  return window.x == (screen.w // 2) + screen.x and
+         window.y == screen.y and
+         window.w == screen.w // 2 and
+         window.h == screen.h
+end
+
+local is_right_two_thirds = function(window, screen)
+  return window.x == ((screen.w // 3) + screen.x) and
+         window.y == screen.y and
+         window.w == ((screen.w // 3) * 2) and
+         window.h == screen.h
+end
+
+-- Resize a window vs a screen.
+
+local resize_full_screen = function(window, screen)
+  window.x = screen.x
+  window.y = screen.y
+  window.w = screen.w
+  window.h = screen.h
+  return window
+end
+
+local resize_left_half = function(window, screen)
+  window.x = screen.x
+  window.y = screen.y
+  window.w = screen.w // 2
+  window.h = screen.h
+  return window
+end
+
+local resize_left_third = function(window, screen)
+  window.x = screen.x
+  window.y = screen.y
+  window.w = screen.w // 3
+  window.h = screen.h
+  return window
+end
+
+local resize_left_two_thirds = function(window, screen)
+  window.x = screen.x
+  window.y = screen.y
+  window.w = (screen.w // 3) * 2
+  window.h = screen.h
+  return window
+end
+
+local resize_right_half = function(window, screen)
+  window.x = (screen.w // 2) + screen.x
+  window.y = screen.y
+  window.w = screen.w // 2
+  window.h = screen.h
+  return window
+end
+
+local resize_right_third = function(window, screen)
+  window.x = (screen.w // 3) * 2 + screen.x
+  window.y = screen.y
+  window.w = screen.w // 3
+  window.h = screen.h
+  return window
+end
+
+local resize_right_two_thirds = function(window, screen)
+  window.x = (screen.w // 3) + screen.x
+  window.y = screen.y
+  window.w = (screen.w // 3) * 2
+  window.h = screen.h
+  return window
+end
+
+-- Run the correct resize function for a user request.
+
+local left_half = function(windowFrame, screenFrame)
+  if is_left_half(windowFrame, screenFrame) then
+    return resize_left_two_thirds(windowFrame, screenFrame)
+  elseif is_left_two_thirds(windowFrame, screenFrame) then
+    return resize_left_third(windowFrame, screenFrame)
+  else
+    return resize_left_half(windowFrame, screenFrame)
+  end
+end
+
+local next_display = function(windowFrame, screenFrame)
+  return resize_full_screen(windowFrame, hs.window.focusedWindow():screen():next():frame())
+end
+
+local full_screen = function(windowFrame, screenFrame)
+  return resize_full_screen(windowFrame, screenFrame)
+end
+
+local right_half = function(windowFrame, screenFrame)
+  if is_right_half(windowFrame, screenFrame) then
+    return resize_right_two_thirds(windowFrame, screenFrame)
+  elseif is_right_two_thirds(windowFrame, screenFrame) then
+    return resize_right_third(windowFrame, screenFrame)
+  else
+    return resize_right_half(windowFrame, screenFrame)
+  end
+end
+
+-- Bind shortcut hotkeys for window resizing.
+
+for _, hotkey in ipairs({
+  { key = 'h', func = left_half },
+  { key = 'n', func = next_display },
+  { key = 'e', func = full_screen },
+  { key = 'i', func = right_half },
+}) do
+  hs.hotkey.bind({'cmd', 'alt'}, hotkey.key, function()
+    local window = hs.window.focusedWindow()
+    local newFrame = hotkey.func(window:frame(), window:screen():frame())
+    window:setFrame(newFrame)
+  end)
+end
