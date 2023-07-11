b367400de393402dc7f4c28478399491e71d9854
Gibson Fahnestock <gibfahn@gmail.com>
2023-07-11 09:45:21 +0100

    build(zsh): make p10k update script leave merge conflict markers
    Otherwise if the merge doesn't apply cleanly then the changes are
    reverted.

--- up/manual/update_p10k.zsh
+++ up/manual/update_p10k.zsh
@@ -13,11 +13,11 @@ p10k_path=dotfiles/.config/zsh/early/p10k.zsh
 previous_stock_update=$(git rev-parse ":/update to latest stock p10k.zsh")
 current_commit=$(git rev-parse @)
 
-echo -e "# Updated by up/manual/update_p10k.zsh script.\n" > $p10k_path
-curl -L https://raw.githubusercontent.com/romkatv/powerlevel10k/master/config/p10k-lean.zsh >> $p10k_path
+echo -e "# Updated by up/manual/update_p10k.zsh script.\n" >$p10k_path
+curl -L https://raw.githubusercontent.com/romkatv/powerlevel10k/master/config/p10k-lean.zsh >>$p10k_path
 git add $p10k_path
 git commit -m "feat(zsh): update to latest stock p10k.zsh version"
 
-git diff $previous_stock_update $current_commit -- $p10k_path | git apply
+git diff ${previous_stock_update:?} ${current_commit:?} -- ${p10k_path:?} | git apply -3
 git add $p10k_path
 git commit -m "fix(zsh): re-apply p10k.zsh modifications"
