af946042b75d907bc31d951332617d527395e7f0
Gibson Fahnestock <gibfahn@gmail.com>
2023-11-22 17:41:02 +0000

    fix(nvim): still allow flash with s in normal mode
    Complexifies the mental model again, but it's annoying not to easily be
    able to move about.

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -80,8 +80,9 @@ return {
       },
     },
     keys = {
+      { "s",     mode = { "n" },           function() require("flash").jump() end,              desc = "Flash" },
       { "S",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "<a-s>",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "<a-s>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
       { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
