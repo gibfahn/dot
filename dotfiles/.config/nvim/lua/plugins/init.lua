ab07120e366738706c28c5c0e1f66304a6d66f6d
Gibson Fahnestock <gibfahn@gmail.com>
2024-02-16 02:39:04 +0000

    fix(nvim): disable start screen / splash screen / dashboard-nvim
    When I open vim with no args I want to be dropped into a blank buffer. I
    never use any of the other options in the splash screen, so disable it.

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -12,12 +12,13 @@ return {
 
   -- Disable lazyvim plugins I don't use.
   -- https://www.lazyvim.org/configuration/plugins#-disabling-plugins
+  { "catppuccin", enabled = false }, -- I have my own colorscheme.
   { "echasnovski/mini.surround", enabled = false }, -- I use kylechui/nvim-surround instead.
   { "folke/persistence.nvim", enabled = false }, -- I don't use sessions.
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false }, -- I use oil.nvim instead.
   { "folke/tokyonight.nvim", enabled = false }, -- I have my own colorscheme.
-  { "catppuccin", enabled = false }, -- I have my own colorscheme.
   { "lukas-reineke/headlines.nvim", enabled = false }, -- Don't need fancy markdown highlighting.
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false }, -- I use oil.nvim instead.
+  { "nvimdev/dashboard-nvim", enabled = false }, -- Don't use the splash/start screen.
 
   { "echasnovski/mini.splitjoin", config = true },
 
