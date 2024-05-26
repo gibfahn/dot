6cbc7f90f9fe8f6c061e14ac9732a52d5710e66c
Gibson Fahnestock <gibfahn@gmail.com>
2024-05-26 09:33:14 +0100

    Revert "fix(hammerspoon): remove key remapping"
    This reverts commit 16a690144d8c9457204441ec74e3142bf75ced86.
    
    It's useful to have this applied via hammerspoon too.

---
+++ dotfiles/.config/hammerspoon/hidutil.lua
@@ -0,0 +1,62 @@
+local log = hs.logger.new("hidutil", "debug")
+log.d("Loading module")
+
+-- Run key remapping script.
+-- This is also run by a LaunchAgent, but sometimes that fails, so keep this as a backup.
+RemapKeys = function()
+  log.d("Remapping keys...")
+  hs.task
+    .new(os.getenv("HOME") .. "/bin/hid", function(exitCode, stdOut, stdErr)
+      if exitCode == 0 then
+        hs.notify
+          .new({
+            title = "✅ Key Remap succeeded",
+            informativeText = exitCode .. " " .. stdOut .. " " .. stdErr,
+            withdrawAfter = 3,
+          })
+          :send()
+      else
+        hs.notify
+          .new({
+            title = "❌ Key Remap failed",
+            informativeText = exitCode .. " " .. stdOut .. " " .. stdErr,
+            withdrawAfter = 20,
+          })
+          :send()
+      end
+
+      hs.hid.capslock.set(false) -- Turn off Caps Lock.
+    end)
+    :start()
+end
+
+-- Always re-remap keys on some events.
+RemapKeyWatcher = hs.caffeinate.watcher.new(function(event)
+  local eventsToMatch = {
+    [hs.caffeinate.watcher.systemDidWake] = true, -- 0
+    [hs.caffeinate.watcher.systemWillSleep] = false, -- 1
+    [hs.caffeinate.watcher.systemWillPowerOff] = false, -- 2
+    [hs.caffeinate.watcher.screensDidSleep] = false, -- 3
+    [hs.caffeinate.watcher.screensDidWake] = false, -- 4
+    [hs.caffeinate.watcher.sessionDidResignActive] = false, -- 5
+    [hs.caffeinate.watcher.sessionDidBecomeActive] = true, -- 6
+    [hs.caffeinate.watcher.screensaverDidStart] = false, -- 7
+    [hs.caffeinate.watcher.screensaverWillStop] = false, -- 8
+    [hs.caffeinate.watcher.screensaverDidStop] = false, -- 9
+    [hs.caffeinate.watcher.screensDidLock] = false, -- 10
+    [hs.caffeinate.watcher.screensDidUnlock] = true, -- 11
+  }
+  -- Check event number against commented numbers above.
+  log.d("Event: ", event, " matches: ", eventsToMatch[event])
+  if eventsToMatch[event] then
+    log.d("RemapKeyWatcher found matching event: " .. event)
+    RemapKeys()
+  end
+end)
+RemapKeyWatcher:start()
+
+-- {{{ §/CapsLock -> Run hidutil script to re-apply key mappings.
+-- These keys are both keys I don't use, that only exist pre-mapping.
+hs.hotkey.bind({}, "§", RemapKeys)
+hs.hotkey.bind({}, "capslock", RemapKeys)
+-- }}} §/CapsLock -> Run hidutil script to re-apply key mappings.
--- dotfiles/.config/hammerspoon/init.lua
+++ dotfiles/.config/hammerspoon/init.lua
@@ -8,6 +8,8 @@ end)
 local home_dir = os.getenv("HOME")
 local user = os.getenv("USER")
 
+require("hidutil")
+
 local isWrkMachine = false
 for file in hs.fs.dir(home_dir) do
   if file == "wrk" then
@@ -33,6 +35,9 @@ if user == "gib" or user == "brian" then
   end
 end
 require("window-management")
+
+-- Always remap keys on first loading hammerspoon.
+RemapKeys()
 
 -- Always connect to VPN on first loading Hammerspoon.
 if isWrkMachine then
