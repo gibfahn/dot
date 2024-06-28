ec8d3bace553a13ff1ffa6a086b17703c07f7200
Gibson Fahnestock <gibfahn@gmail.com>
2024-06-28 14:06:33 +0100

    feat(nvim): add typo detection
    Using the typos CLI, which works pretty well.

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -343,6 +343,11 @@ return {
         python = { "pylint", "mypy" },
         zsh = { "zsh" },
         bash = { "shellcheck" },
+        -- Use the "*" filetype to run linters on all filetypes.
+        -- ['*'] = { 'global linter' },
+        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        -- ['_'] = { 'fallback linter' },
+        ["*"] = { "typos" },
       },
       linters = {
         -- https://github.com/mfussenegger/nvim-lint#custom-linters
