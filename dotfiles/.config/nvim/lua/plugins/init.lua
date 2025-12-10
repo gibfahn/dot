939c6f72b73d8e013bff0847d093eb909c7d878c
Gibson Fahnestock <gibfahn@gmail.com>
2025-12-10 11:32:25 +0000

    feat(nvim): add more status bar elements

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -722,9 +722,31 @@ return {
               end,
             },
 
+            -- stylua: ignore
+            Snacks.profiler.status(),
+            -- stylua: ignore
             {
-              require("noice").api.status.mode.get, -- Show @recording when recording a macro.
-              cond = require("noice").api.status.mode.has,
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = function() return { fg = Snacks.util.color("Statement") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = function() return { fg = Snacks.util.color("Constant") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = function() return { fg = Snacks.util.color("Debug") } end,
+            },
+            -- stylua: ignore
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = function() return { fg = Snacks.util.color("Special") } end,
             },
           },
           lualine_z = {
